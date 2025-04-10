e1c29036ed9b670b829b4d9b5ea25eab972609d6
DiegoLorga <ramseslorga@gmail.com>
2025-04-10 12:50:26 -0600

    Gizelle Ramirez: (Commit desde esta cuenta porque mi computadora esta en reparacion) agregando modelo de base datos y modelosde cada tabla.

---
+++ tareas/models.py
@@ -0,0 +1,14 @@
+
+from django.db import models
+
+
+class Tarea(models.Model):
+    # En lugar de importar el modelo:
+    usuarioid = models.ForeignKey('registro.Estudiante', on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=100)
+    estado = models.IntegerField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
