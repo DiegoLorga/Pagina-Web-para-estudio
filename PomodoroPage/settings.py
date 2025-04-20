746ee4113294edd0b6657471a956bac402c95fad
aneli1 <148299361+aneli1@users.noreply.github.com>
2025-04-20 09:56:30 -0600

    Implementando asistente con PDFs
    Aneli Arce Jmz, implementando un asistente con llama3 que ayude a estudiar con pdfs

--- PomodoroPage/settings.py
+++ PomodoroPage/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'principal',
     'registro',
-    'login'
+    'login',
+    'llamaapp'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'llamaapp/static')]
