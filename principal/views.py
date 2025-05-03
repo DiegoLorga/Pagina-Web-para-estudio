b1214e0633e8b2808b9c3d82a1f1f026ad684ffb
DiegoLorga <ramseslorga@gmail.com>
2025-05-02 19:16:48 -0600

    Creacion de tareas, correcion en modelo de tareas, nuevas funciones para crear, actualizar listas

--- principal/views.py
+++ principal/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect
+from tareas.models import Tarea  # Asegúrate que esta línea esté presente
 
 def menu_principal(request):
     usuario_id = request.session.get('usuario_id')  # recupera el id del usuario logueado
     if not usuario_id:
         return redirect('login')  # si no está logueado, mándalo a login
 
-    return render(request, 'menu_principal.html', {'usuario_id': usuario_id}) 
+    # Obtener las tareas del usuario
+    tareas = Tarea.objects.filter(usuarioid_id=usuario_id).order_by('-fecha_creacion')
+
+    return render(request, 'menu_principal.html', {
+        'usuario_id': usuario_id,
+        'tareas': tareas
+    })
